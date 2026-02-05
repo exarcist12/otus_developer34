8fac38b744002dc24bf26868cf227a6ef77c3d2f
Stanislav <Stanislav.Petrov@russianpost.ru>
2026-02-05 20:24:16 +0400

    добавлен запуск с 2 конфигов

--- hw-di25/src/main/java/ru/otus/App.java
+++ hw-di25/src/main/java/ru/otus/App.java
@@ -2,8 +2,10 @@ package ru.otus;
 
 import ru.otus.appcontainer.AppComponentsContainerImpl;
 import ru.otus.appcontainer.api.AppComponentsContainer;
-import ru.otus.config.AppConfig;
+import ru.otus.config.AppConfig2;
+import ru.otus.config.AppConfig3;
 import ru.otus.services.GameProcessor;
+import ru.otus.services.GameProcessorImpl;
 
 /*
 В классе AppComponentsContainerImpl реализовать обработку, полученной в конструкторе конфигурации,
@@ -24,17 +26,17 @@ public class App {
 
     public static void main(String[] args) {
         // Опциональные варианты
-        // AppComponentsContainer container = new AppComponentsContainerImpl(AppConfig1.class, AppConfig2.class);
+        AppComponentsContainer container = new AppComponentsContainerImpl(AppConfig2.class, AppConfig3.class);
 
         // Тут можно использовать библиотеку Reflections (см. зависимости)
         // AppComponentsContainer container = new AppComponentsContainerImpl("ru.otus.config");
 
         // Обязательный вариант
-        AppComponentsContainer container = new AppComponentsContainerImpl(AppConfig.class);
+        // AppComponentsContainer container = new AppComponentsContainerImpl(AppConfig.class);
 
         // Приложение должно работать в каждом из указанных ниже вариантов
-        GameProcessor gameProcessor = container.getAppComponent(GameProcessor.class);
-        // GameProcessor gameProcessor = container.getAppComponent(GameProcessorImpl.class);
+        // GameProcessor gameProcessor = container.getAppComponent(GameProcessor.class);
+        GameProcessor gameProcessor = container.getAppComponent(GameProcessorImpl.class);
         // GameProcessor gameProcessor = container.getAppComponent("gameProcessor");
 
         gameProcessor.startGame();
--- hw-di25/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
+++ hw-di25/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -16,8 +16,14 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private final Map<String, Integer> orderNames = new HashMap<>();
 
+    private final Map<Class<?>, List<Object>> appComponentsByType = new HashMap<>();
+
     public AppComponentsContainerImpl(Class<?> initialConfigClass) {
         processConfig(initialConfigClass);
+    }
+
+    public AppComponentsContainerImpl(Class<?>... initialConfigClasses) {
+        processConfig(initialConfigClasses);
     }
 
     private void processConfig(Class<?> configClass) {
@@ -27,11 +33,17 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
         try {
             Object configInstance = configClass.getDeclaredConstructor().newInstance();
-
+            Set<String> usedNames = new HashSet<>();
             for (Method method : methods) {
-                int orderM = method.getAnnotation(AppComponent.class).order();
-                String name = method.getAnnotation(AppComponent.class).name();
-                orderNames.put(name, orderM);
+                if (method.isAnnotationPresent(AppComponent.class)) {
+                    String name = method.getAnnotation(AppComponent.class).name();
+                    if (usedNames.contains(name)) {
+                        throw new IllegalStateException();
+                    }
+                    usedNames.add(name);
+                    int orderM = method.getAnnotation(AppComponent.class).order();
+                    orderNames.put(name, orderM);
+                }
             }
 
             List<Map.Entry<String, Integer>> orderNamesSorted = orderNames.entrySet().stream()
@@ -42,8 +54,12 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
             for (int i = 0; i < orderNamesSorted.size(); i++) {
                 String nameMethod = orderNamesSorted.get(i).getKey();
+
                 Method m = Arrays.stream(methods)
-                        .filter(method -> method.getName().equals(nameMethod))
+                        .filter(method -> {
+                            AppComponent annotation = method.getAnnotation(AppComponent.class);
+                            return annotation != null && annotation.name().equals(nameMethod);
+                        })
                         .findFirst()
                         .get();
 
@@ -55,14 +71,111 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
                     Object[] args = new Object[parameters.length];
                     for (int j = 0; j < parameters.length; j++) {
                         Parameter parameter = parameters[j];
-                        String parName = parameter.getName();
-                        args[j] = getAppComponent(parName);
+                        var paramType = parameter.getType();
+                        args[j] = getAppComponent(paramType);
                     }
                     result = m.invoke(configInstance, args);
                 }
 
                 appComponents.add(result);
                 appComponentsByName.put(nameMethod, result);
+                var componentType = m.getReturnType();
+                appComponentsByType
+                        .computeIfAbsent(componentType, k -> new ArrayList<>())
+                        .add(result);
+                for (var iface : componentType.getInterfaces()) {
+                    appComponentsByType
+                            .computeIfAbsent(iface, k -> new ArrayList<>())
+                            .add(result);
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void processConfig(Class<?>... configClasses) {
+        for (Class<?> configClass : configClasses) {
+            checkConfigClass(configClass);
+        }
+
+        try {
+            for (Class<?> configClass : configClasses) {
+                Method[] methods = configClass.getDeclaredMethods();
+                Set<String> usedNames = new HashSet<>();
+
+                for (Method method : methods) {
+                    if (method.isAnnotationPresent(AppComponent.class)) {
+                        String name = method.getAnnotation(AppComponent.class).name();
+                        if (usedNames.contains(name)) {
+                            throw new IllegalStateException();
+                        }
+                        usedNames.add(name);
+                        int orderM = method.getAnnotation(AppComponent.class).order();
+                        orderNames.put(name, orderM);
+                    }
+                }
+            }
+
+            List<Map.Entry<String, Integer>> orderNamesSorted = orderNames.entrySet().stream()
+                    .sorted(Map.Entry.comparingByValue())
+                    .collect(Collectors.toList());
+
+            System.out.println("orderNamesSorted = " + orderNamesSorted);
+
+            for (int i = 0; i < orderNamesSorted.size(); i++) {
+                String nameMethod = orderNamesSorted.get(i).getKey();
+
+                Method m = null;
+                Object configInstanceForMethod = null;
+
+                for (Class<?> configClass : configClasses) {
+                    Method[] methods = configClass.getDeclaredMethods();
+                    Optional<Method> foundMethod = Arrays.stream(methods)
+                            .filter(method -> {
+                                AppComponent annotation = method.getAnnotation(AppComponent.class);
+                                return annotation != null && annotation.name().equals(nameMethod);
+                            })
+                            .findFirst();
+
+                    if (foundMethod.isPresent()) {
+                        m = foundMethod.get();
+                        configInstanceForMethod =
+                                configClass.getDeclaredConstructor().newInstance();
+                        break;
+                    }
+                }
+
+                if (m == null) {
+                    throw new RuntimeException("Method not found for component: " + nameMethod);
+                }
+
+                Parameter[] parameters = m.getParameters();
+                Object result;
+                if (parameters.length == 0) {
+                    result = m.invoke(configInstanceForMethod);
+                } else {
+                    Object[] args = new Object[parameters.length];
+                    for (int j = 0; j < parameters.length; j++) {
+                        Parameter parameter = parameters[j];
+                        var paramType = parameter.getType();
+                        args[j] = getAppComponent(paramType);
+                    }
+                    result = m.invoke(configInstanceForMethod, args);
+                }
+
+                appComponents.add(result);
+                appComponentsByName.put(nameMethod, result);
+                var componentType = m.getReturnType();
+                appComponentsByType
+                        .computeIfAbsent(componentType, k -> new ArrayList<>())
+                        .add(result);
+                for (var iface : componentType.getInterfaces()) {
+                    appComponentsByType
+                            .computeIfAbsent(iface, k -> new ArrayList<>())
+                            .add(result);
+                }
             }
 
         } catch (Exception e) {
@@ -72,23 +185,33 @@ public class AppComponentsContainerImpl implements AppComponentsContainer {
 
     private void checkConfigClass(Class<?> configClass) {
         if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
-            throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
+            throw new IllegalArgumentException();
         }
     }
 
     @Override
     public <C> C getAppComponent(Class<C> componentClass) {
+        List<Object> foundComponents = new ArrayList<>();
+
         for (Object component : appComponentsByName.values()) {
             if (componentClass.isAssignableFrom(component.getClass())) {
-                return (C) component;
+                foundComponents.add(component);
             }
         }
-        throw new RuntimeException("Component not found for type: " + componentClass);
+
+        if (foundComponents.isEmpty() || foundComponents.size() > 1) {
+            throw new RuntimeException();
+        }
+
+        return (C) foundComponents.get(0);
     }
 
     @Override
     public <C> C getAppComponent(String componentName) {
         Object component = appComponentsByName.get(componentName);
+        if (component == null) {
+            throw new RuntimeException();
+        }
         return (C) component;
     }
 }
